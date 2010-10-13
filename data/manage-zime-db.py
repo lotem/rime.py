50de5d9bd4894149fce0700ff929f69926ebd7f0
lotem <chen.sst@gmail.com>
2010-10-13 23:42:12 +0800

    refactoring zimedb, extracted module zimealgebra. (in progress)

--- data/manage-zime-db.py
+++ data/manage-zime-db.py
@@ -69,6 +69,7 @@ DROP TABLE IF EXISTS %(prefix)s_kb;
 CLEAR_SETTING_VALUE_SQL = """
 DELETE FROM setting_values WHERE path_id IN (SELECT id FROM setting_paths WHERE path LIKE :path);
 """
+
 CLEAR_SETTING_PATH_SQL = """
 DELETE FROM setting_paths WHERE path LIKE :path;
 """
@@ -85,6 +86,27 @@ FROM setting_paths p LEFT JOIN setting_values v ON p.id = v.path_id
 WHERE path LIKE '%/Dict';
 """
 
+QUERY_PHRASE_SQL = """
+SELECT id FROM phrases WHERE phrase = :phrase;
+"""
+
+ADD_PHRASE_SQL = """
+INSERT INTO phrases VALUES (NULL, :phrase);
+"""
+
+ADD_UNIGRAM_SQL = """
+INSERT INTO %(prefix)s_unigram VALUES (NULL, :p_id, :okey, 0, :freq);
+"""
+
+QUERY_UNIGRAM_SQL = """
+SELECT id FROM %(prefix)s_unigram WHERE p_id = :p_id AND okey = :okey;
+"""
+
+QUERY_UNIGRAM_SQL = """
+SELECT u.id FROM %(prefix)s_unigram u LEFT JOIN phrases p ON p_id = p.id 
+WHERE phrase = :phrase AND okey = :okey;
+"""
+
 QUERY_USER_FREQ_SQL = """
 SELECT phrase, ufreq, okey
 FROM %(prefix)s_unigram u, phrases p 
@@ -97,9 +119,99 @@ FROM %(prefix)s_bigram b, %(prefix)s_unigram u1, %(prefix)s_unigram u2, phrases 
 WHERE bfreq > 0 AND e1 = u1.id AND e2 = u2.id AND u1.p_id = p1.id AND u2.p_id = p2.id
 """
 
+INC_USER_FREQ_SQL = """
+UPDATE %(prefix)s_unigram SET ufreq = ufreq + :n WHERE id = :id;
+"""
+
+QUERY_KEY_SQL = """
+SELECT id FROM %(prefix)s_keys WHERE ikey = :ikey;
+"""
+
+ADD_KEY_SQL = """
+INSERT INTO %(prefix)s_keys VALUES (NULL, :ikey);
+"""
+
+UPDATE_STATS_SQL = """
+UPDATE %(prefix)s_stats SET ufreq = ufreq + :freq;
+"""
+
+INC_UFREQ_SQL = """
+UPDATE %(prefix)s_unigram SET ufreq = ufreq + :freq WHERE id = :id;
+"""
+
+BIGRAM_EXIST_SQL = """
+SELECT rowid FROM %(prefix)s_bigram WHERE e1 = :e1 AND e2 = :e2;
+"""
+
+INC_BFREQ_SQL = """
+UPDATE %(prefix)s_bigram SET bfreq = bfreq + :n WHERE e1 = :e1 AND e2 = :e2;
+"""
+
+ADD_BIGRAM_SQL = """
+INSERT INTO %(prefix)s_bigram VALUES (:e1, :e2, 1);
+"""
+
+QUERY_KB_SQL = """
+SELECT rowid FROM %(prefix)s_kb WHERE k_id = :k_id AND b_id = :b_id;
+"""
+
+ADD_KB_SQL = """
+INSERT INTO %(prefix)s_kb VALUES (:k_id, :b_id);
+"""
+
 def clear_schema_setting(path):
     cur.execute(CLEAR_SETTING_VALUE_SQL, {'path' : path})
     cur.execute(CLEAR_SETTING_PATH_SQL, {'path' : path})
+
+def get_or_insert_key(key):
+    args = {'ikey' : u' '.join(key)}
+    r = None
+    while not r:
+        r = cur.execute(QUERY_KEY_SQL, args).fetchone()
+        if not r:
+            cur.execute(ADD_KEY_SQL, args)
+    return r[0]
+
+def get_or_insert_phrase(phrase):
+    args = {'phrase' : phrase}
+    r = None
+    while not r:
+        r = cur.execute(QUERY_PHRASE_SQL, args).fetchone()
+        if not r:
+            cur.execute(ADD_PHRASE_SQL, args)
+    return r[0]
+
+def inc_userfreq(phrase, okey, freq, prefix_args):
+
+    def add_phrase(args):
+        cur.execute(ADD_UNIGRAM_SQL, args)
+        # generate index for the new phrase
+        key_ids = [get_or_insert_key(k) for k in g([[]], okey.split(), 0)]
+        if not key_ids:
+            print >> sys.stderr, 'failed index generation for phrase [%s] %s.' % (okey, phrase)
+        for k_id in key_ids:
+            add_ku(k_id, u_id)
+
+    # increment userfreq in table *_unigram
+    p_id = get_or_insert_phrase(phrase)
+    args = {'p_id' : p_id, 'okey' : okey, 'freq' : freq}
+    r = cur.execute(QUERY_UNIGRAM_SQL % prefix_args, args).fetchone()
+    if not r:
+        print >> sys.stderr, 'INFO: introducing new phrase', phrase, okey
+        add_phrase(args)
+        r = cur.execute(QUERY_UNIGRAM_SQL, args).fetchone()
+    elif freq > 0:
+        cur.execute(INC_UFREQ_SQL, args)
+
+    # update userfreq total in table *_stats
+    if freq > 0:
+        cur.execute(UPDATE_STATS_SQL % prefix_args, {'freq': freq})
+
+    print >> sys.stderr, 'DEBUG: restored userfreq', phrase, okey, freq
+
+def inc_usergram(phrase1, okey1, phrase2, okey2, freq, prefix_args):
+    print >> sys.stderr, 'TODO: restore usergram', phrase1, okey1, phrase2, okey2, freq
+    pass
 
 
 usage = 'usage: %prog [options] [command]'
@@ -182,9 +294,11 @@ def save_dict(prefix):
         return
 
     filename_prefix = prefix.replace(u'_', u'-')
+    prefix_args = {'prefix' : prefix}
+
     userfreq_file = "%s-userfreq.txt" % filename_prefix
     out = open(userfreq_file, "w")
-    r = cur.execute(QUERY_USER_FREQ_SQL % {'prefix' : prefix}).fetchall()
+    r = cur.execute(QUERY_USER_FREQ_SQL % prefix_args).fetchall()
     for x in r:
         print >> out, (u"%s\t%d\t%s" % tuple(x)).encode('utf-8')
     out.close()
@@ -192,7 +306,7 @@ def save_dict(prefix):
 
     usergram_file = "%s-usergram.txt" % filename_prefix
     out = open(usergram_file, "w")
-    r = cur.execute(QUERY_USER_GRAM_SQL % {'prefix' : prefix}).fetchall()
+    r = cur.execute(QUERY_USER_GRAM_SQL % prefix_args).fetchall()
     for x in r:
         print >> out, (u"%s\t%s\t%d\t%s\t%s" % tuple(x)).encode('utf-8')
     out.close()
@@ -203,8 +317,44 @@ def restore_dict(prefix):
     if prefix not in prefixes:
         print >> sys.stderr, u'non-existing dict: %s' % prefix
         return
-    print >> sys.stderr, 'TODO: restore_dict not implemented'
-    return
+
+    filename_prefix = prefix.replace(u'_', u'-')
+    prefix_args = {'prefix' : prefix}
+
+    userfreq_file = "%s-userfreq.txt" % filename_prefix
+    for line in open(userfreq_file):
+        x = line.strip().decode('utf-8').lstrip(u'\ufeff')
+        if not x or x.startswith(u'#'):
+            continue
+        try:
+            ll = x.split(u'\t', 2)
+            (phrase, freq_str, okey) = ll
+            freq = int(freq_str)
+            if u' ' in phrase:
+                phrase = phrase.replace(u' ', '')
+        except:
+            print >> sys.stderr, 'error: invalid format (%s) %s' % (userfreq_file, x)
+            exit()
+        inc_userfreq(phrase, okey, freq, prefix_args)
+
+    usergram_file = "%s-usergram.txt" % filename_prefix
+    for line in open(usergram_file):
+        x = line.strip().decode('utf-8').lstrip(u'\ufeff')
+        if not x or x.startswith(u'#'):
+            continue
+        try:
+            ll = x.split(u'\t', 4)
+            (phrase1, phrase2, freq_str, okey1, okey2) = ll
+            freq = int(freq_str)
+            if u' ' in phrase1:
+                phrase1 = phrase1.replace(u' ', '')
+            if u' ' in phrase2:
+                phrase2 = phrase2.replace(u' ', '')
+        except:
+            print >> sys.stderr, 'error: invalid format (%s) %s' % (usergram_file, x)
+            exit()
+        inc_usergram(phrase1, okey1, phrase2, okey2, freq, prefix_args)
+
     if options.compact:
         cur.execute("""VACUUM;""")
     conn.commit()
